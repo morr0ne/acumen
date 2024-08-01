73cbce7dd0dac44cdfcd3495e7e599cae6986a40
Matilde Morrone <contact@morrone.dev>
2024-08-01 13:13:07 +0200

    Add parser for meminfo

--- examples/info.rs
+++ examples/info.rs
@@ -1,9 +1,11 @@
-use acumen::{Cpuinfo, OsRelease};
+use acumen::{Cpuinfo, Meminfo, OsRelease};
 
 fn main() {
     let os = OsRelease::new();
     let cpuinfo = Cpuinfo::new();
+    let meminfo = Meminfo::new();
 
     dbg!(os);
     dbg!(cpuinfo);
+    dbg!(meminfo);
 }
